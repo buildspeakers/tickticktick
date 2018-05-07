2386896ed2e1a227aa7bc6df20337d5bacdfcf87
buildspeakers <sudobuildspeakers@gmail.com>
2018-05-07 01:00:56 +0100

    setup gulp/scss/browser-sync

--- dist/js/app.js
+++ dist/js/app.js
@@ -204,3 +204,39 @@ function filterTodos() {
 
   bindTodoEvents();
 }
+
+
+// Storage / state
+let todos = [];
+let view = 'ALL';
+
+// input field
+let todoInput = qs(".todo-add__input");
+
+// Initial event listeners
+
+// Add todo
+qs('.todo-button__add').addEventListener('click', addTodo);
+// add event when enter key pressed
+todoInput.addEventListener('keyup', function (event) {
+  if (event.which === 13) {
+    addTodo();
+  }
+});
+
+// Filter todos
+qs('.filter__all').addEventListener('click', function () {
+  view = 'ALL';
+  swapActiveClass(this);
+  filterTodos();
+});
+qs('.filter__incomplete').addEventListener('click', function () {
+  view = 'INCOMPLETE';
+  swapActiveClass(this);
+  filterTodos();
+});
+qs('.filter__complete').addEventListener('click', function () {
+  view = 'COMPLETE';
+  swapActiveClass(this);
+  filterTodos();
+});
